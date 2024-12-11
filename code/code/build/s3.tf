8ccb1e017adea0ad4057ed9681d15fae61d53fd9
darkhorse1212 <56946955+darkhorse1212@users.noreply.github.com>
2024-12-11 12:48:16 -0500

    Update s3.tf

--- code/code/build/s3.tf
+++ code/code/build/s3.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "dev_s3-am" {
+resource "aws_s3_bucket" "dev_s3-avm" {
   bucket_prefix = "dev-"
 
   tags = {
     Environment          = "Dev"
-    yor_name             = "dev_s3-am"
+    yor_name             = "dev_s3-avm"
     yor_trace            = "fe83a2cc-0061-4a7b-9ecd-94bd9bd34cf3"
     git_commit           = "42964d9802504b595b965462e2ea055833424649"
     git_file             = "code/code/build/s3.tf"
@@ -19,7 +19,7 @@ resource "aws_s3_bucket" "dev_s3-am" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "dev_s3-am" {
+resource "aws_s3_bucket_ownership_controls" "dev_s3-avm" {
   bucket = aws_s3_bucket.dev_s3-am.id
   rule {
     object_ownership = "BucketOwnerPreferred"
